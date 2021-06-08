aaf45d574a9465240f5b3f2d461de3efa54321ee
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-08 11:53:32 +0100

    Update invite log + check.py

--- cogs/invite log + check.py
+++ cogs/invite log + check.py
@@ -5,7 +5,7 @@ import async
 @bot.command(name='invite', description='Creates an invite link for the server')
 async def invite(ctx, reason):
     invite = await ctx.guild.create_invite(reason=reason)
-    await.ctx.author.send(str(invite)) #This will send the invite link to the user who asked for it
+    await ctx.author.send(str(invite)) #This will send the invite link to the user who asked for it
     invite.inviter = ctx.author
     bot.dispatch('invite_command', invite)
 
