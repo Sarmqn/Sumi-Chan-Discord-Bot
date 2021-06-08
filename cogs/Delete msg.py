ef055ff7a749ee4b561eb042ab2e299bd08ba261
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-08 11:57:01 +0100

    Update Delete msg.py

--- cogs/Delete msg.py
+++ cogs/Delete msg.py
@@ -2,7 +2,7 @@ import discord from discord.ext
 import async
 
 async def on_message(message):
-    if 'discord.gg' in message.content:
+    if 'discord.gg/' in message.content:
         await message.delete()
         await message.chnnel.send(f"{message.author.mention} This is forbidden to send!")
     else:
