bc72ba52aac28380ffee8fa426c50554fe073664
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-09 11:12:43 +0100

    Update running the bot.py

--- cogs/running the bot.py
+++ cogs/running the bot.py
@@ -1,13 +1,10 @@
-import os
-import time
 import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="sumi!", case_insentive = True) #Bot prefix
 
 @bot.event
-asynd def on_ready():
+async def on_ready():
     print("Ready to work") #Says in console whether bot is ready to recieve commands
-
-        
+   
 bot.run('TOKEN') #Bot key code
