5f35cbbceaf9ef2cd9fc63ec35aa31bc56b17d65
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-09 11:57:31 +0100

    Update On.py

--- cogs/On.py
+++ cogs/On.py
@@ -1,10 +1,18 @@
 import discord
 from discord.ext import commands
+from pretty_help import Navigation, PrettyHelp
+from asyncio import sleep
+import os
+import random
 
-bot = commands.Bot(command_prefix="sumi!", case_insentive = True) #Bot prefix
+bot = commands.Bot(command_prefix="sc!", case_insentive = True, description = "Nice handy bot that will help around") #Bot prefix
+bot.help_command = PrettyHelp(navigation=nav, color=discord.Colour.green()
 
 @bot.event
 async def on_ready():
-    print("Ready to work") #Says in console whether bot is ready to recieve commands
-   
-bot.run('TOKEN') #Bot key code
+    print("Ready to work haha") #Says in console whether bot is ready to recieve commands
+def run():
+    bot.run('TOKEN') #Bot token code
+                              
+if __name__ == "__main__":
+  run()   
