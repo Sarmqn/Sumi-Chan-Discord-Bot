ece43ccaf939ce49e38bdbfa104eea54aa7ef538
Dawood <83209168+14685-562@users.noreply.github.com>
2021-06-10 14:09:06 +0100

    Update Delete msg.py
    Fixed "chnnl" and grammar

--- cogs/Delete msg.py
+++ cogs/Delete msg.py
@@ -4,7 +4,7 @@ import async
 async def on_message(message):
     if 'discord.gg/' in message.content:
         await message.delete()
-        await message.chnnel.send(f"{message.author.mention} This is forbidden to send!")
+        await message.channel.send(f"This is forbidden to send {message.author.mention}!")
     else:
         await bot.process_commands(message)
         
