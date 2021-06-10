5eb3f73a70ab8717c32848281c9f2d31cc60d745
Dawood <83209168+14685-562@users.noreply.github.com>
2021-06-10 14:12:36 +0100

    Update Joins.py

--- cogs/Joins.py
+++ cogs/Joins.py
@@ -7,7 +7,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_messge_join(member):
     channel = client.get_channel(channel_id)
-    embed.discord.Embed(title=f"Welcome {member.name}", description = f"Thanks for joining the server! {member.guild.name}!")
-    embed.set_thumbnail(url=member.avata_url) # Embed's thumbnail = Users PFP
+    JoinEmbed = discord.Embed(title=f"Welcome {member}", description = f"Thanks for joining {member.guild.name}!")
+    JoinEmbed.set_thumbnail(url=member.avata_url) # Embed's thumbnail = Users PFP
 
-    await channel.send(embed=embed)
+    await channel.send(embed=JoinEmbed)
