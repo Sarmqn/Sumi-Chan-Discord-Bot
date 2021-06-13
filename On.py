ea3e68625327e45f0e964167b4065c053d52f1b7
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-13 12:48:35 +0100

    Create On.py

---
+++ On.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from pretty_help import Navigation, PrettyHelp
+from asyncio import sleep
+import os
+import random
+
+bot = commands.Bot(command_prefix="sc!", case_insentive = True, description = "Nice handy bot that will help around") #Bot prefix
+bot.help_command = PrettyHelp(navigation=nav, color=discord.Colour.DARK_VIVID_PINK)
+
+@bot.event
+async def on_ready():
+    print("Ready to work haha") # Lets the bot owner know when the bot is ready
+
+bot.run('TOKEN')
