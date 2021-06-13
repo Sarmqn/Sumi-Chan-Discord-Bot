f738db6ec1834f23ed06fdba8e671c894616d647
Adam Crawley <37088720+adampy@users.noreply.github.com>
2021-06-13 13:22:50 +0100

    Update On.py

--- On.py
+++ On.py
@@ -1,11 +1,13 @@
 import discord
 from discord.ext import commands
-from pretty_help import PrettyHelp
+from pretty_help import PrettyHelp, DefaultMenu
 from asyncio import sleep
 import os
 import random
 
 bot = commands.Bot(command_prefix="sc!", case_insentive = True, description = "Nice handy bot that will help around") #Bot prefix
+
+nav = DefaultMenu('◀️', '▶️', '❌')
 bot.help_command = PrettyHelp(navigation=nav, color=discord.Colour.DARK_VIVID_PINK)
 
 @bot.event
