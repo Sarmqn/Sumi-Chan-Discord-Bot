2382bf834ec4abe609c21951b4d73d025b96aa78
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-13 17:28:48 +0100

    Update Music.py

--- cogs/Music.py
+++ cogs/Music.py
@@ -4,24 +4,31 @@ from discord.ext import commands
 
 players = {}
 
-@commands.Cog.listener(pass_context=True, aliases = ["Connect", "JoinVC"])
-async def Join(ctx):
-    channel = ctx.message.author.voice.voice_channel
-    await client.join_Voice_Channel(channel)
+class Music(commands.Cog):
+    """
+    Moderation commands/listeners for log channels
+    """
+    def __init__(self, bot):
+        self.bot = bot
+        
+        @commands.Cog.listener(pass_context=True, aliases = ["Connect", "JoinVC"]
+                               async def Join(ctx):
+                               channel = ctx.message.author.voice.voice_channel
+                               await client.join_Voice_Channel(channel)
 
-@commands.Cog.listener(pass_context=True, aliases = ["fuckoff", "dc", "disconnect", "LeaveVC"])
-async def leave(ctx):
-    server = ctx.message.server
-    Voice_client = client.voice_channel_in(server)
-    await Voice_client.disconnect()
+        @commands.Cog.listener(pass_context=True, aliases = ["fuckoff", "dc", "disconnect", "LeaveVC"])
+                               async def leave(ctx):
+                               server = ctx.message.server
+                               Voice_client = client.voice_channel_in(server)
+                               await Voice_client.disconnect()
 
-@commands.Cog.listener(pass_context=True, aliases = ["p"])
-async def play(ctx, url):
-    server = ctx.message.server
-    Voice_client = client.Voice_client_in(server)
-    player = await Voice_client.create.ytdl_player(url)
-    players[server.id] = player
-    player.start()
+        @commands.Cog.listener(pass_context=True, aliases = ["p"])
+                               async def play(ctx, url):
+                               server = ctx.message.server
+                               Voice_client = client.Voice_client_in(server)
+                               player = await Voice_client.create.ytdl_player(url)
+                               players[server.id] = player
+                               player.start()
 
     def setup(bot):
     bot.add_cog(Music(bot))
