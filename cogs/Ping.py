64ea46665e5bd85663fadeb35af37701185de3fb
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-13 13:43:02 +0100

    Update Ping.py

--- cogs/Ping.py
+++ cogs/Ping.py
@@ -2,6 +2,6 @@ import time
 import os
 import discord
 
-@bot.command(name = 'Ping', description= "Checks the current ping")
+@bot.command(name = 'ping', description= "Checks the current ping")
 async def ping(ctx):
     await ctx.send(f'Pong! Bot latency is {round(bot.latency * 1000)}ms') # Says in chat what the current ping is and rounds it to the nearest whole number
