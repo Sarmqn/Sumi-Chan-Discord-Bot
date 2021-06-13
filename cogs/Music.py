54ec60118e17cf03af2dda84a0371707ab5e5f3d
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-13 17:23:56 +0100

    Create Music.py

---
+++ cogs/Music.py
@@ -0,0 +1,27 @@
+from discord import voice_client
+from discord.channel import VoiceChannel
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+
+players = {}
+
+@commands.Cog.listener(pass_context=True, aliases = ["Connect", "JoinVC"])
+async def Join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_Voice_Channel(channel)
+
+@commands.Cog.listener(pass_context=True, aliases = ["fuckoff", "dc", "disconnect", "LeaveVC"])
+async def leave(ctx):
+    server = ctx.message.server
+    Voice_client = client.voice_channel_in(server)
+    await Voice_client.disconnect()
+
+@commands.Cog.listener(pass_context=True)
+async def play(ctx, url):
+    server = ctx.message.server
+    Voice_client = client.Voice_client_in(server)
+    player = await Voice_client.create.ytdl_player(url)
+    players[server.id] = player
+    player.start()
