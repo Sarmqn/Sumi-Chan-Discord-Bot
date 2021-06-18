9ef06621dff4833297ab86fa0d2a1047e2d5df2a
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-18 11:49:55 +0100

    Updated leave command
    Leave command has been updated, need to test it though

--- cogs/music.py
+++ cogs/music.py
@@ -1,9 +1,16 @@
 from discord import voice_client
 from discord.channel import VoiceChannel
-from discord.ext import commands
+from discord.ext import commands,tasks
+from dotenv import load_dotenv
+import youtube_dl
+import os
 
 
+load_dotenv()
 players = {}
+
+intents = discord.Intents().all.()
+Client = discord.Client(intents=intents)
 
 class music(commands.Cog):
     """
@@ -19,9 +26,11 @@ class music(commands.Cog):
 
     @commands.command(aliases = ["fuckoff", "dc", "disconnect", "LeaveVC"])
     async def leave(self, ctx):
-        server = ctx.guild
-        channel = ctx.author.voice.channel
-        Voice_client = await channel.disconnect()
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_connected():
+            await voice_client.disconnect()
+        else:
+            await ctx.send("The bot is not currently connected to a voice channel.")
 
     @commands.command(aliases = ["p"])
     async def play(self, ctx, url):
@@ -29,6 +38,16 @@ class music(commands.Cog):
         player = await Voice_client.create.ytdl_player(url)
         players[server.id] = player
         player.start()
-
+        
+    @commands.command(aliases = ["Pause", "halt])
+    async def pause(self, ctx):
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        voice_channel.pause()
+        print('Pause')
+   
+   @commands.command(aliases = ["Stop", "Discontinue", "end"])
+   async def stop(self, ctx
+                                 
 def setup(bot):
     bot.add_cog(music(bot))
