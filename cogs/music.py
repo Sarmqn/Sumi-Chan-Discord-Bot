59a87f7708899dac0b0f0ec733be52fa9c691a9e
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-06-18 11:59:30 +0100

    Additions to Music.py
    Have added a stop, resume and pause command, needs testing though

--- cogs/music.py
+++ cogs/music.py
@@ -44,10 +44,21 @@ class music(commands.Cog):
         server = ctx.message.guild
         voice_channel = server.voice_client
         voice_channel.pause()
-        print('Pause')
-   
+        print('The song has been paused')
+   @commands.command(aliases = ["restart", "Resume"])
+   async def resume(self, ctx):
+       voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not previously playing anything to resume!")
+                       
    @commands.command(aliases = ["Stop", "Discontinue", "end"])
-   async def stop(self, ctx
-                                 
+   async def stop(self, ctx):
+       voice_client = ctx.message.guild.voice_client
+       if voice_client.is_plying():
+           await voice_clien.stop()
+       else:
+           await ctx.send('There was not anything playing earlier to stop')                      
 def setup(bot):
     bot.add_cog(music(bot))
