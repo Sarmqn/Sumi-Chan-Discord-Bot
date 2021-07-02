0c3315f4d3763eb4edf004c8bab33be7d9a7530f
Adam Crawley <adamflint8@hotmail.co.uk>
2021-07-02 14:12:37 +0100

    Fixed general cmds

--- cogs/general.py
+++ cogs/general.py
@@ -22,7 +22,7 @@ class General(commands.Cog):
         await ctx.send(f'Pong! Bot latency is {round(self.bot.latency * 1000)}ms') # Says in chat what the current ping is and rounds it to the nearest whole number
 
     @commands.command(name = 'nickname', aliases=["Nickname", "nick"])
-    async def nickname(ctx, member: discord.Member, nick):
+    async def nickname(self, ctx, member: discord.Member, nick):
         """
         Helps user change nickname using command sc!nick or the aliases too
         """
@@ -30,14 +30,14 @@ class General(commands.Cog):
         await ctx.send(f"{member.mention}'s nickname has been changed!") # prints and pings the user that changed nickname
     
     @commands.command(name='server')
-    async def fetchServerInfo(context):
-        guild = context.guild
-        await context.send(f'Server Name: {guild.name}')
-        await context.send(f'Owner Name: {guild.owner.display_name}')
-        await context.send(f'Server Size: {len(guild.members)}')
+    async def fetchServerInfo(self, ctx):
+        guild = ctx.guild
+        await ctx.send(f'Server Name: {guild.name}')
+        await ctx.send(f'Owner Name: {guild.owner.display_name}')
+        await ctx.send(f'Server Size: {len(guild.members)}')
     
     @commands.Cog.listener()
-    async def on_message(message):
+    async def on_message(self, message):
          if message.context == "test":
                 await message.channel.send("Testing 1, 2, 3!")
          if message.context == "hello":
