c7ebb7b61f3af010c45054e8352b9f9495aad835
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-07-08 14:43:48 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -97,8 +97,13 @@ class Logs(commands.Cog):
                 await ctx.send(f"**{member}** was unmuted.")
             else:
                 await ctx.send(f'**{member}** is not muted.')
+       
+    @commands.command('Unban') #Unban command
+    async def unban(self,ctx, member:discord.Member):
+        if ctx.author.guild_permissions.ban_members==True:
+            userID = await bot.fetch_user(id)
+            await ctx.guild.unban(userID)
 
-
-
+        
 def setup(bot):
     bot.add_cog(Logs(bot))
