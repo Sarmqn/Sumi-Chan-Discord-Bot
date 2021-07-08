9479f6656292965b25b8945820f2d59d4759790d
Dawood <83209168+14685-562@users.noreply.github.com>
2021-07-08 14:44:00 +0100

    Updated help
    Added the list of help commands.
    Edited the command with up to date information.
    Added random embed colours.

--- cogs/help.py
+++ cogs/help.py
@@ -1,35 +1,43 @@
 import discord
+from discord.ext import commands
+from random import randint
+helplist = [['ban', 'Bans a user.'], ['mute', 'Indefinitely mutes a user.'], ['unmute', 'Unmutes a muted user.'], ['invite', 'Creates an invite link for the server.'], ['ping', "Checks the bot's ping."], ['server', 'Sends information about the server.'], ['disconnect', 'Disconnects the user's voice channel.'], ['join', 'Joins the voice channel of the user.'], ['pause', 'Pauses the currently playing song.'], ['play', 'Plays a song!'], ['resume', 'Resumes the currently playing song!']]
 
-@client.command()
-async def plshelp(ctx, *hargs):
-    hargs = ''.join(hargs)
-    if (str(ctx.message.author.id) == '364045258004365312') or (str(ctx.message.author.id) == '389897179701182465'):
-        if hargs == '':
-            HelpEmbed=discord.Embed(title='Help for MinjuMail!', description='This message will self-destruct in 1 minute so as to not take up too much space.', color=random.choice(embedcolours))
-            HelpEmbed.add_field(name="Admin", value="say\nshutdown", inline=True)
-            HelpEmbed.add_field(name="Utility", value="reportabug", inline=True)
-            HelpEmbed.add_field(name="Miscellaneous", value="ping\npong\nsnow", inline=True)
-            HelpMessage = await ctx.send(embed=HelpEmbed)
-        else:
-            found = False
-            for i in helplist:
-                if i[0] == f'{hargs}':
-                    index = helplist.index(i)
-                    found = True
-                    break
-                else:
-                    pass
-            if found == False:
-                HelpMessage = await ctx.send(embed=discord.Embed(title='Misspelling?', description=f'I could not find {hargs} command in the list of commands {ctx.author.mention}!'))
+
+class Help(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command('help')
+    async def help(self, ctx, *hargs):
+        hargs = ''.join(hargs)
+            if hargs == '':
+                HelpEmbed=discord.Embed(title=f'Help for {ctx.guild.me.display_name}!', description='This message will self-destruct in 2 minutes so as to not take up too much space.', color=discord.Color.from_rgb(randint(0, 255),randint(0, 255),randint(0, 255)))
+                if member.guild_permissions.administrator==True:
+                    HelpEmbed.add_field(name="Admin", value="ban\nmute\nunmute", inline=True)
+                HelpEmbed.add_field(name="Utility", value="invite\nping\nserver", inline=True)
+                HelpEmbed.add_field(name="Music", value="disconnect\njoin\npause\nplay\nresume", inline=True)
+                HelpMessage = await ctx.send(embed=HelpEmbed)
             else:
-                HelpEmbed=discord.Embed(title=f"Help for '{hargs}'!", description='', color=random.choice(embedcolours))
-                HelpEmbed.add_field(name="Description", value=f'{helplist[index][1]}', inline=False)
-                HelpEmbed.add_field(name="Aliases", value=f'{helplist[index][2]}', inline=False)
-                HelpEmbed.add_field(name="Command example", value=f'_{helplist[index][0]}', inline=False)
-            HelpMessage = await ctx.send(embed=HelpEmbed)
-        
-        await ctx.message.delete()
-        await asyncio.sleep(60)
-        await HelpMessage.delete()
-    else:
-        pass
+                found = False
+                for i in helplist:
+                    if i[0] == f'{hargs}':
+                        index = helplist.index(i)
+                        found = True
+                        break
+                    else:
+                        pass
+                if found == False:
+                    HelpMessage = await ctx.send(embed=discord.Embed(title='Misspelling?', description=f'I could not find {hargs} command in the list of commands {ctx.author.mention}!'))
+                else:
+                    HelpEmbed=discord.Embed(title=f"Help for '{hargs}'!", description='', color=discord.Color.from_rgb(randint(0, 255),randint(0, 255),randint(0, 255)))
+                    HelpEmbed.add_field(name="Description", value=f'{helplist[index][1]}', inline=False)
+                HelpMessage = await ctx.send(embed=HelpEmbed)
+
+            await ctx.message.delete()
+            await asyncio.sleep(120)
+            await HelpMessage.delete()
+
+
+def setup(bot):
+    bot.add_cog(Help(bot))
