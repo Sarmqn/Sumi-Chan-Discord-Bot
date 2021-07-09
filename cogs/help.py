42ed72cc68612ea3658673d3f9df4719f295fc88
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-07-09 11:23:13 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -16,9 +16,9 @@ class Help(commands.Cog):
                 HelpEmbed=discord.Embed(title=f'Help for {ctx.guild.me.display_name}!', description='This message will self-destruct in 2 minutes so as to not take up too much space.', color=discord.Color.from_rgb(randint(0, 255),randint(0, 255),randint(0, 255)))
                 if member.guild_permissions.administrator==True:
                     HelpEmbed.add_field(name="Admin", value="ban\nmute\nunmute", inline=True)
-                HelpEmbed.add_field(name="Utility", value="invite\nping\nserver", inline=True)
-                HelpEmbed.add_field(name="Music", value="disconnect\njoin\npause\nplay\nresume", inline=True)
-                HelpMessage = await ctx.send(embed=HelpEmbed)
+                    HelpEmbed.add_field(name="Utility", value="invite\nping\nserver", inline=True)
+                    HelpEmbed.add_field(name="Music", value="disconnect\njoin\npause\nplay\nresume", inline=True)
+                    HelpMessage = await ctx.send(embed=HelpEmbed)
                 else:
                     found = False
                 for i in helplist:
