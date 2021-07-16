521c9775003e640bc743c659f0d420857206b8a4
Mikey <mikey@mikeyo.ml>
2021-07-16 12:21:52 -0400

    🔥 remove bot initiation that was in general

--- cogs/general.py
+++ cogs/general.py
@@ -3,11 +3,6 @@ import discord
 from discord.ext import commands
 import discord
 
-# --Load intents--
-intents = discord.Intents.default()
-intents.members = True
-bot = commands.Bot(command_prefix="sc!", case_insentive = True, description = "Nice handy bot that will help around", intents=intents) #Bot prefix
-
 class General(commands.Cog):
     """
     General commands that server members can use
@@ -16,11 +11,12 @@ class General(commands.Cog):
         self.bot = bot
 
     @commands.command(name = 'ping')
-    async def ping(self, ctx):
+    async def ping(self, ctx: commands.Context):
         """
         Checks the current ping for the bot
         """
-        await ctx.send(f'Pong! Bot latency is {round(self.bot.latency * 1000)}ms') # Says in chat what the current ping is and rounds it to the nearest whole number
+        embed = self.bot.embed(title="Pong!", description=f"Bot latency is {round(self.bot.latency * 1000)}ms") # this works because of the custom class
+        await ctx.reply(embed=embed)
 
     @commands.command(name = 'nickname', aliases=["Nickname", "nick"])
     async def nickname(self, ctx, member: discord.Member, nick):
@@ -29,14 +25,14 @@ class General(commands.Cog):
         """
         await member.edit(nick=nick)
         await ctx.send(f"{member.mention}'s nickname has been changed!") # prints and pings the user that changed nickname
-    
+
     @commands.command(name='server')
     async def server_info(ctx: commands.Context):
         guild = ctx.guild
         await ctx.send(f'Server Name: {guild.name}')
         await ctx.send(f'Owner Name: {guild.owner.display_name}')
         await ctx.send(f'Server Size: {len(guild.members)}')
-    
+
     @commands.Cog.listener()
     async def on_message(self, message):
          if message.content == "test":
