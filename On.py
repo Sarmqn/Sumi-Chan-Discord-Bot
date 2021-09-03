53f9cc3327255c8f82c4f01f4b88dcc216503fe5
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-09-03 23:46:38 +0100

    Monké Mongo DB
    Added mongo database, not finished yet owo

--- On.py
+++ On.py
@@ -38,6 +38,16 @@ for cog in cogs:
     bot.load_extension("cogs." + cog)
     print("Loaded: " + cog)
 
+if __name__ == "__main__":
+    bot.mongo = motor.motor_asyncio.AsyncIOMotorClient(Str(bot.connection_url))
+    bot.db = bot.mongo["Documents"]
+    bot.config = Document(bot.db, "config")
+    bot.reaction_roles = Document(bot.db, "Reaction_Roles")
+    
+for file in os.listdir(cmd + "/cogs"):
+    if file.endswith(".py") and not file.startswith("_"):
+        bot.load_extension(f"cogs.{file[:-3]}")
+    
 # --Start bot--
 bot_token = os.environ.get("TOKEN")
 bot.run(bot_token)
