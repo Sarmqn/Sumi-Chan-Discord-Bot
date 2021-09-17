ecec573fb4cf95fcc094179312288f08eaa74621
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-09-17 08:55:16 +0100

    Updated DRP + Comments
    Adding comments to code to explain what it does and changed DRP

--- On.py
+++ On.py
@@ -1,3 +1,4 @@
+# --Importing all necessary libraries--
 import discord
 from discord.ext import commands
 from pretty_help import PrettyHelp, DefaultMenu
@@ -7,17 +8,19 @@ import os
 intents = discord.Intents.default()
 intents.members = True
 
+# --Class for my bot Sumi-Chan--
 class SumiChan(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="sc!", description="Nice handy bot that will help around", intents=intents, help_command=None)
+        super().__init__(command_prefix="sc!", description="Nice handy bot that will help around", intents=intents, help_command=None) # Super class
 
 bot = SumiChan()
 
 @bot.event
-async def on_ready():
-    print("Online") # Lets the bot owner know when the bot is ready
-    await bot.change_presence(activity = discord.Activity(type=discord.ActivityType.listening, name='Matane!'))
-# --Load cogs--
+async def on_ready(): # When the bot turns on
+    print("Roaring to go") # Lets the bot owner know when the bot is ready, it will print out that it's "Roaring to go".
+    await bot.change_presence(activity = discord.Game(name="Matane!")) # A Discord Rich Presense that will say the bot is playing "Matane!"
+    "await bot.change_presence(activity = discord.Activity(type=discord.ActivityType.listening, name='Matane!'))" # A Discord Rich Presense that will say, commented out to try a new one
+# --Loading all cogs--
 cogs = [
     "moderation",
     "general",
@@ -29,7 +32,7 @@ for cog in cogs:
     print("Loaded: " + cog)
 
     """
-    
+#Database use currently commented out until finished    
 if __name__ == "__main__":
     bot.mongo = motor.motor_asyncio.AsyncIOMotorClient(Str(bot.connection_url))
     bot.db = bot.mongo["Documents"]
