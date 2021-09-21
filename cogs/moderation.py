4630d49da96e3f290330c97f973d23b3c48f3ce0
Dawood <83209168+14685-562@users.noreply.github.com>
2021-09-21 12:50:27 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -11,29 +11,27 @@ class Logs(commands.Cog): # Creates the class with an instance of Logs
     def __init__(self, bot):
         self.bot = bot
         self.log_channel_id = 699909552757276732
+        self.log_channel = bot.get_channel(699909552757276732)
 
     @commands.Cog.listener() # Delete any invite links from the chat
     async def on_message(self, message): 
-        if not message.author.bot and 'discord.gg/' in message.content:
+        if not message.author.bot and ('discord.gg/' in message.content) or ('discord.com/invite/' in message.content):
             await message.delete()
-            await message.channel.send(f"This is forbidden to send within this server {message.author.mention}!")
+            await message.channel.send(f"Don't send server invites in this server {message.author.mention}!")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        guild = self.bot.guilds[0]
-        channel = self.bot.get_channel(self.log_channel_id)
         JoinEmbed = discord.Embed(title=f"Welcome {member}", description = f"Thanks for joining {member.guild.name}!")
         JoinEmbed.set_thumbnail(url=member.avatar_url) # Embed's thumbnail = Users PFP
-        await channel.send(embed=JoinEmbed)
+        await self.log_channel.send(embed=JoinEmbed)
         role = discord.utils.get(member.guild.roles, id=678551601740251136) #gets an object when given certain criteria and a source to look from
         await member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        channel = self.bot.get_channel(self.log_channel_id)
         LeaveEmbed = discord.Embed(title=f"See you next time, {member}", description = f"Thanks for being a part of {member.guild.name}!")
         LeaveEmbed.set_thumbnail(url=member.avatar_url)
-        await channel.send(embed=LeaveEmbed)
+        await self.log_channel.send(embed=LeaveEmbed)
         
     @commands.command(name='invite')
     @commands.guild_only() # Restricts the command to the guild only
@@ -41,11 +39,10 @@ class Logs(commands.Cog): # Creates the class with an instance of Logs
         """
         Creates an invite link for the server
         """
-        invite = await ctx.channel.create_invite()
+        invite = await ctx.channel.create_invite(reason=f"{ctx.author} used the invite command.", max_uses = 1, unique=True)
         await ctx.author.send(str(invite)) #This will send the invite link to the user who asked for it
-        loggingchannel = self.bot.get_channel(self.log_channel_id)
         embed = discord.Embed(title= "New Invite", description=f"Invite created by {ctx.author}\nCode: {str(invite)}")
-        await loggingchannel.send(embed=embed) #Logs who created the invite link
+        await self.log_channel.send(embed=embed) #Logs who created the invite link
         
         #  ---BAN---
         
@@ -66,7 +63,10 @@ class Logs(commands.Cog): # Creates the class with an instance of Logs
                 return await ctx.send("Whoops! You can't ban them...")
             else:
                 await member.send(f"You were banned from **{ctx.guild}** by **{ctx.author}**.")
+                await ctx.send(f"{member} has been banned.")
+                await self.log_channel.send(f"{member} has been banned by {ctx.author}.")
                 await member.ban()
+                
 
                 
         #  ---MUTE---    
