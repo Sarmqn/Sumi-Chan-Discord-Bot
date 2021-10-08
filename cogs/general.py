263c2d4d087745c920d6302106b184091531a88d
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-10-08 12:16:27 +0100

    quick fix?

--- cogs/general.py
+++ cogs/general.py
@@ -21,7 +21,7 @@ class General(commands.Cog):
     
     # --Getting information about the server--
     @commands.command(name='server')
-    async def server_info(ctx: commands.Context): # When server_info is in instance:
+    async def server_info(self, ctx: commands.Context): # When server_info is in instance:
         guild = ctx.guild # Collect data about the server
         guildOwner = bot.get_user(int(ctx.guild.owner.id))
         await ctx.send(f'Server Owner: {guildOwner}')
