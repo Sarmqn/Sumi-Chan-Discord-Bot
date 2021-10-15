871f6631ca07abffcd931d098b2c17bab42a5cb7
Dawood <83209168+14685-562@users.noreply.github.com>
2021-10-15 10:00:25 +0100

    Changed code. Check description (1/2)
    Line 8: Edited the comment
    Line 17: Edited the comment
    Line 22: Added a line to log it.
    Line 26: Edited punctuation
    Line 27: Edited comment
    Line 29: Edited comment to be less vague
    Line 30: Added a comment
    Line 38: Removed `'invite'`
    Line 46: Edited message
    Line 50: Removed `'ban'`
    Line 51: Added a check for the ban permissions.
    Line 52: Added a `reason` argument.
    Line 53: Removed the if statement checking for ban permissions. Added a comment.
    Line 57: Added a check to make sure the member is valid.
    Line 72: Added a reason.
    Line 74: Added a reason.
    Line 75: Added a reason.
    Line 77 & 78: Removed unused lines.
    Line 78: Removed `Shut up` alias. It won't work.
    Line 79: Added decorator to specifically check for manage messages, mute, and roles perms.
    Line 81: Removed if statement for admin perms.
    Line 93: Added same decorator.
    Line 95: Removed if statement.
    Line 104: Added `# ---UNBAN---` to follow the format.
    Line 106: Added ban perms check.
    Line 107: Added a space. Added reason argument.
    Line 108: Removed if statement. Edited comment.
    Line 110: Added reason.
    Line 114: Added reason.
    Line 118: Added `# ---KICK---` to fit format.
    Line 120: Added decorator check.
    Line 122: Removed if statement.
    Line 128: Edited message.
    Line 131: Added `# ---PURGE---` to fit format.
    Line 134: Changed arg to allow multiple words.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -5,7 +5,7 @@ import discord.utils
 from discord.utils import get
 import asyncio
 
-class Logs(commands.Cog): # Creates the class with an instance of Logs
+class Logs(commands.Cog): # Creates a class called "Logs" as a subclass of commands.Cog
     """
     Moderation commands/listeners for log channels
     """
@@ -14,19 +14,20 @@ class Logs(commands.Cog): # Creates the class with an instance of Logs
         self.log_channel_id = 699909552757276732
         self.log_channel = bot.get_channel(699909552757276732)
 
-    @commands.Cog.listener() # Delete any invite links from the chat
+    @commands.Cog.listener() # Detect discord.gg invite links and delete them.
     async def on_message(self, message): 
         if not message.author.bot and ('discord.gg/' in message.content) or ('discord.com/invite/' in message.content):
             await message.delete()
             await message.channel.send(f"Don't send server invites in this server {message.author.mention}!")
+            await self.log_channel.send(f'{message.author.mention} ({message.author.id}) sent an invite in {message.channel}.')
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        JoinEmbed = discord.Embed(title=f"Welcome {member}", description = f"Thanks for joining {member.guild.name}!")
-        JoinEmbed.set_thumbnail(url=member.avatar_url) # Embed's thumbnail = Users PFP
+        JoinEmbed = discord.Embed(title=f"Welcome {member}!", description = f"Thanks for joining {member.guild.name}! We hope you enjoy your stay!")
+        JoinEmbed.set_thumbnail(url=member.avatar_url) # Set the embed's thumbnail to be the user's profile picture.
         await self.log_channel.send(embed=JoinEmbed)
-        role = discord.utils.get(member.guild.roles, id=678551601740251136) #gets an object when given certain criteria and a source to look from
-        await member.add_roles(role)
+        role = discord.utils.get(member.guild.roles, id=678551601740251136) # Gets the role object given an ID and list
+        await member.add_roles(role) # Gives new user the aforementioned role
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
@@ -34,105 +35,103 @@ class Logs(commands.Cog): # Creates the class with an instance of Logs
         LeaveEmbed.set_thumbnail(url=member.avatar_url)
         await self.log_channel.send(embed=LeaveEmbed)
         
-    @commands.command('invite')
+    @commands.command()
     @commands.guild_only() # Restricts the command to the guild only
     async def invite(self, ctx):
         """
         Creates an invite link for the server
         """
         invite = await ctx.channel.create_invite(reason=f"{ctx.author} used the invite command.", max_uses = 1, unique=True)
-        await ctx.author.send(str(invite)) #This will send the invite link to the user who asked for it
-        embed = discord.Embed(title= "New Invite", description=f"Invite created by {ctx.author}\nCode: {str(invite)}")
+        await ctx.author.send(str(invite)) # This will send the invite link to the user who asked for it
+        embed = discord.Embed(title= "New Invite", description=f"Invite created by {ctx.author}\nInvite Link: {str(invite)}")
         await self.log_channel.send(embed=embed) #Logs who created the invite link
         
         #  ---BAN---
-        
-    @commands.command('ban', aliases = ['goaway', 'Ban'])
-    async def ban(self, ctx, member: discord.Member):
-        if ctx.author.guild_permissions.ban_members==True:
-            if isinstance(member, discord.Member):
+    @commands.command(aliases = ['goaway', 'Ban'])
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *reason: str = None):
+        if isinstance(member, discord.Member): # Checks if the argument passed was a discord.Member object.
+            pass
+        elif isinstance(member, int):
+            member = self.bot.get_user(member)
+            if member is None: # If the ID is invalid
+                await ctx.send("That isn't a valid member ID.")
+        else:
+            await ctx.send('Not an ID/mention. Try again.')
+            member = None
+        # If they are trying to ban the bot
+        if member == ctx.guild.me:
+            return await ctx.send("Nice try")
+        # If they are trying to ban someone with administrator permissions
+        if member.guild_permissions.administrator==True:
+            return await ctx.send("Whoops! You can't ban them...")
+        else:
+            if member == None:
                 pass
-            elif isinstance(member, int):
-                member = self.bot.get_user(member)
             else:
-                await ctx.send('Not an ID/mention. Try again.')
-                member = None
-            # If they are trying to ban the bot
-            if member == ctx.guild.me:
-                return await ctx.send("Nice try")
-            if member.guild_permissions.administrator==True:
-                return await ctx.send("Whoops! You can't ban them...")
-            else:
-                if member == None:
-                    pass
-                else:
-                    await member.send(f"You were banned from **{ctx.guild}** by **{ctx.author}**.")
-                    await ctx.send(f"{member} has been banned.")
-                    await self.log_channel.send(f"{member} has been banned by {ctx.author}.")
-                    await member.ban()
-                
-
+                await member.send(f"You were banned from **{ctx.guild}** by **{ctx.author}**.\nReason: {reason}.")
+                await ctx.send(f"{member} has been banned.")
+                await self.log_channel.send(f"{member} has been banned by {ctx.author}.\nReason: {reason}.")
+                await member.ban(reason=reason)
                 
         #  ---MUTE---    
-            
-    @commands.command('mute', aliases = ['Mute', 'Shut up', 'shutup', 'quiet']) # Mute command
+    @commands.command('mute', aliases = ['Mute', 'shutup', 'quiet']) # Mute command
+    @commands.has_permissions(manage_messages=True, manage_roles=True, mute_members=True)
     async def mute(self, ctx, member: discord.Member):
-        if ctx.author.guild_permissions.administrator==True:
-            role_muted = discord.utils.get(ctx.guild.roles, name='Muted')
-            if role_muted in member.roles:
-                await ctx.send(f'**{member}** is already muted.')
-            else:
-                role_members = discord.utils.get(ctx.guild.roles, name='Member')
-                await member.remove_roles(role_members)
-                await member.add_roles(role_muted)
-                await ctx.send(f"**{member}** was muted.")
+        role_muted = discord.utils.get(ctx.guild.roles, name='Muted')
+        if role_muted in member.roles:
+            await ctx.send(f'**{member}** is already muted.')
+        else:
+            role_members = discord.utils.get(ctx.guild.roles, name='Member')
+            await member.remove_roles(role_members)
+            await member.add_roles(role_muted)
+            await ctx.send(f"**{member}** was muted.")
 
                
         #  ---UNMUTE---    
-            
     @commands.command('unmute', aliases = ['Unmute']) # Unmute command
+    @commands.has_permissions(manage_messages=True, manage_roles=True, mute_members=True)
     async def unmute(self, ctx, member: discord.Member):
-        if ctx.author.guild_permissions.administrator==True:
-            role_muted = discord.utils.get(ctx.guild.roles, name='Muted')
-            if role_muted in member.roles:
-                role_members = discord.utils.get(ctx.guild.roles, name='Member')
-                await member.remove_roles(role_muted)
-                await member.add_roles(role_members)
-                await ctx.send(f"**{member}** was unmuted.")
-            else:
-                await ctx.send(f'**{member}** is not muted.')
-    
+        role_muted = discord.utils.get(ctx.guild.roles, name='Muted')
+        if role_muted in member.roles:
+            role_members = discord.utils.get(ctx.guild.roles, name='Member')
+            await member.remove_roles(role_muted)
+            await member.add_roles(role_members)
+            await ctx.send(f"**{member}** was unmuted.")
+        else:
+            await ctx.send(f'**{member}** is not muted.')
+
+        # ---UNBAN---
     @commands.command('unban', aliases = ['Unban', 'comeback']) #Unban command
-    async def unban(self,ctx, id: int):
-        if ctx.author.guild_permissions.ban_members==True:
-            userID = await ctx.self.bot.fetch_user(id) # Gets users ID
-            try:
-                await ctx.guild.unban(userID)
-            except discord.errors.NotFound:
-                await ctx.send('User is not banned!')
-            else:
-                await ctx.send(f'**{userID}** has been unbanned.')
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, id: int, *reason: str = None):
+        userID = await ctx.self.bot.fetch_user(id) # Gets user's ID
+        try:
+            await ctx.guild.unban(userID, reason=reason)
+        except discord.errors.NotFound:
+            await ctx.send('User is not banned!')
         else:
-            pass
+            await ctx.send(f'**{userID}** has been unbanned.\nReason: {reason}.')
+    else:
+        pass
         
-        
+        # ---KICK---
     @commands.command('kick', aliases = ['Kick', 'remove', 'bye']) # Kicks a user that is mentioned
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, user: discord.Member):
-        if ctx.author.guild_permissions.kick_members==True or ctx.author.guild_permissions.administrator==True:
-            if isinstance(user, discord.Member):
-                await user.kick()
-            elif isinstance(user, int) or isinstance(user, str):
-                user = guild.get_member(int(userid))
-                await user.kick()
-            await ctx.message.add_reaction("👌")
-            await ctx.send(f"{user.name} was kicked by {ctx.author.name}!")
-            await user.send(f"You were kicked from **{ctx.guild}** by **{ctx.author}**.")
-        else:
-            pass
+        if isinstance(user, discord.Member):
+            await user.kick()
+        elif isinstance(user, int) or isinstance(user, str):
+            user = guild.get_member(int(userid))
+            await user.kick()
+        await ctx.message.add_reaction("👌")
+        await ctx.send(f"**{user}** was kicked by {ctx.author.name}!")
+        await user.send(f"You were kicked from **{ctx.guild}** by **{ctx.author}**.")
                        
+        # ---PURGE---
     @commands.command('Purge', aliases = ['purge', 'delete',]) # Purge command
     @commands.has_permissions(administrator=True) # checks for admin perms for the user who uses it        
-    async def purge(self, ctx, amount, arg:str=None):
+    async def purge(self, ctx, amount, *arg:str=None):
         await ctx.message.delete() # Deletes messages using the command prefix and the parameter
         await ctx.channel.purge(limit=int(amount)) # Purges messages in the channel based on the inputed amount
         deletemsg = await ctx.send(f"{amount} messages have been deleted from the channel!") # prints a message stating that the messages have been purged
@@ -142,5 +141,3 @@ class Logs(commands.Cog): # Creates the class with an instance of Logs
                                            
 def setup(bot):
     bot.add_cog(Logs(bot))
-
-# Mom's spagghetti
