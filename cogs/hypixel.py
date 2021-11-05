07acb22eaad10f5441e2c708e22ff2a0a9f96c01
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-11-05 21:19:50 +0000

    Update hypixel.py

--- cogs/hypixel.py
+++ cogs/hypixel.py
@@ -7,7 +7,7 @@ class Hypixel(commands.Cog):
 
 def level(player_name: str): # Player Level command
     data = requests.get(url = "https://api.hypixel.net/player", params = {"key": API_KEY,"name": player_name}).json() # Create an api get request to recieve data about the players account
-    await ctx.send(data)
+    return data
     
 def setup(bot):
     bot.add_cog(Hypixel(bot))
