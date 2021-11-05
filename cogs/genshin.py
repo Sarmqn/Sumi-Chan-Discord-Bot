09c579db7e86a5fcec7764dc31b6e22fe805cf42
Dawood <83209168+14685-562@users.noreply.github.com>
2021-11-05 16:56:20 +0000

    Might skill

--- cogs/genshin.py
+++ cogs/genshin.py
@@ -30,17 +30,36 @@ class Genshin(commands.Cog, name='<:GenshinImpact:905489184205197322> Genshin Im
                 embed.add_field(name='Skills', value=f'Use `sc!genshin skills {character}`', inline=True)
                 embed.add_field(name='Constellations', value=f'Use `sc!genshin constellation {character}`', inline=True)
             elif response.status_code == 404:
-                embed = discord.Embed(title='Character Profiles', description='That person does not exist! Please make sure you typed it correctly!', color=discord.Color.from_rgb(200,0,0))
-                # embed.set_thumbnail(url=f'https://api.genshin.dev/characters/{character}/icon-big') Need to change the URL to EHE TE NANDAYO
+                embed = discord.Embed(title='Character Profiles', description='That person does not exist! Please make sure you typed their name correctly!', color=discord.Color.from_rgb(200,0,0))
+                embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/737096050598346866/906223201166704640/ehe_te_nandayo.png')
             else:
                 embed = discord.Embed(title='Character Profiles', description='Uh oh, an error has occured!\nThe developer has been informed and will work on this issue ASAP!', color=discord.Color.from_rgb(200,0,0))
                 self.bot.get_user(221188745414574080).send(f"There was a {response.status_code} code from the Genshin API in the character command.\nArguments: {character}")
         embed.set_author(name='Character Profiles', icon_url=ctx.author.avatar_url)
-        await ctx.reply(embed=embed)
+        await ctx.reply(embed=embed, mention_author=False)
 
-    @genshin.command(aliases=['s', 'skill'], description="Look at a character's skills.")
-    async def skills(self, ctx, * character: str):
-        await ctx.reply('Hi!')
+    @genshin.command(aliases=['s', 'skill', 't', 'talents'], description="Look at a character's skills.")
+    async def skills(self, ctx, character: str = None):
+        if character is None:
+            embed = discord.Embed(title='Character Profiles', description="Learn about a Genshin characters' skills! For a list of available characters use `sc!genshin characters`.')
+        else:
+            character = character.lower()
+            response = requests.get(f'https://api.genshin.dev/characters/{character}/')
+            if response.status_code == 200:
+                skills = response.json()['skillTalents']
+                for i in skills:
+                    upgradesText = ''
+                    for j in i['upgrades']:
+                        upgradesText += f"{j['name']}: {j['value']}"
+                    embed.add_field(name=f"{i['name']} ({i['unlock'})", value=f"{i['description']}\n**Upgrades:**\n{upgradesText}", inline=True)
+            elif response.status_code == 404:
+                embed = discord.Embed(title='Character Profiles', description='That person does not exist! Please make sure you typed their name correctly!', color=discord.Color.from_rgb(200,0,0))
+                embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/737096050598346866/906223201166704640/ehe_te_nandayo.png')
+            else:
+                embed = discord.Embed(title='Character Profiles', description='Uh oh, an error has occured!\nThe developer has been informed and will work on this issue ASAP!', color=discord.Color.from_rgb(200,0,0))
+                self.bot.get_user(221188745414574080).send(f"There was a {response.status_code} code from the Genshin API in the character command.\nArguments: {character}")
+        embed.set_author(name='Character Profiles', icon_url=ctx.author.avatar_url)
+        await ctx.reply(embed=embed, mention_author=False)
 
     @genshin.command(aliases=['co'], description="Look at a character's constellation.")
     async def constellation(self, ctx, * character: str):
