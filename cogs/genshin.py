b677655c8eb4d5193b107461e83372c78bd1642a
Dawood <83209168+14685-562@users.noreply.github.com>
2021-11-09 12:19:09 +0000

    Update genshin.py

--- cogs/genshin.py
+++ cogs/genshin.py
@@ -184,11 +184,13 @@ class Genshin(commands.Cog, name='<:GenshinImpact:905489184205197322> Genshin Im
         food = ' '.join(food)
         if food == '':
             response = requests.get("https://api.genshin.dev/consumables/food/")
+            print(response)
             response = response.json()
             print(response)
+            print(type(response))
             foodstr = ""
             for i in response:
-                print(i)
+                foodstr += f"{i}, "
             foodstr = foodstr[:-2]
             embed = discord.Embed(title='List of All Food', description=foodstr, colour=discord.Color.from_rgb(241,210,231))
         else:
