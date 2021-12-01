4ceea1bad28fef5324b6bd69459143bace84607e
Dawood <83209168+14685-562@users.noreply.github.com>
2021-12-01 18:09:02 +0000

    Update On.py

--- On.py
+++ On.py
@@ -23,7 +23,7 @@ async def on_ready(): # When the bot turns on
 @bot.command()
 @commands.is_owner()
 async def testactiv(id: int):
-    print(bot.get_user(id).activities)
+    print(ctx.guild.get_member(id).activities)
 
 
 # --Loading all cogs--
