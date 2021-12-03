38c0aa6dc082bf8bb05fb7aefd71b97793bd6716
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-12-03 12:42:57 +0000

    added some aliases

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -8,12 +8,12 @@ class Miscellaneous (commands.Cog, name = "Miscellaneous"):
   def __init__ (self, bot):
     self.bot = bot
     
-    @commands.command(description="Displays the user's profile picture!")
+    @commands.command(description="Displays the user's profile picture!", aliases = ["dp", "av", "pfp"])
     async def avatar(self, ctx, *, dp: discord.Member=None):
       URL = dp.avatar_url
       await ctx.send(URL)
       
-    @commands.command(decription = "Changes the nickname for the tagged user within this server!")
+    @commands.command(decription = "Changes the nickname for the tagged user within this server!", aliases = ["nickname", "Nick", "changename", "name")
     async def nick(ctx, nick, member: discord.Member=None):
       await member.edit(nick=nick)
       await ctx.send(f"{member.mention}'s nickname in {ctx.guild.name} has been changed")
