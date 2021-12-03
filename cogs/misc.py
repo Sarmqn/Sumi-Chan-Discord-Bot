551f8ddce6d6779a0620939061b6e6c4db2bd990
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-12-03 16:09:10 +0000

    comments

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,24 +1,24 @@
 import discord
 from discord.ext import commands
 
-class Miscellaneous(commands.Cog, name = " 👻 Miscellaneous"):
+class Miscellaneous(commands.Cog, name = " 👻 Miscellaneous"): # Creating a class called Miscellaneous, for misc commands
   """
   Miscellaneous commands
   """
-  def __init__ (self, bot):
+  def __init__ (self, bot): # Constructor Method
     self.bot = bot
     
-  @commands.command(description="Displays the user's profile picture!", aliases = ["dp", "av", "pfp"])
-  async def avatar(self, ctx, *, member: discord.Member=None):
-    if not member: 
-      member = ctx.author
-    URL = member.avatar_url
-    await ctx.send(URL)
+  @commands.command(description="Displays the user's profile picture!", aliases = ["dp", "av", "pfp"]) 
+  async def avatar(self, ctx, *, member: discord.Member=None): # Avatar command
+    if not member: #If user is not a member
+      member = ctx.author # member is the user who sent the message
+    URL = member.avatar_url # Takes the url of the member's profile picture
+    await ctx.send(URL) # Sends the url to post in chat
       
   @commands.command(decription = "Changes the nickname for the tagged user within this server!", aliases = ["nickname", "changename", "name"])
-  async def nick(ctx, nick, member: discord.Member=None):
-    await member.edit(nick=nick)
-    await ctx.send(f"{member.mention}'s nickname in {ctx.guild.name} has been changed")
+  async def nick(ctx, nick, member: discord.Member=None): # Nickname command
+    await member.edit(nick=nick) # member edits nickname
+    await ctx.send(f"{member.mention}'s nickname in {ctx.guild.name} has been changed") # send in chat that nickname has been changed
       
       
       
