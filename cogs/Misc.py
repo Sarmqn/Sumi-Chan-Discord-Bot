4591d61542b634546096d2d40a56f1e158ff53b4
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-12-03 12:26:09 +0000

    New miscellaneous commands
    New set of commands hopefully they work 😳

---
+++ cogs/Misc.py
@@ -0,0 +1,24 @@
+import discord
+from discord import commands
+
+class Micellaneous (commands.Cog, name = "Miscellaneous"):
+  """
+  Miscellaneous commands
+  """
+  def __init__ (self, bot):
+    self.bot = bot
+    
+    @commands.command(description="Displays the user's profile picture!")
+    async def avatar(self, ctx, *, dp: discord.Member=None):
+      URL = dp.avatar_url
+      await ctx.send(URL)
+      
+    @commands.command(decription = "Changes the nickname for the tagged user within this server!")
+    async def nick(ctx, nick, member: discord.Member=None):
+      await member.edit(nick=nick)
+      await ctx.send(f"{member.mention}'s nickname in {ctx.guild.name} has been changed")
+      
+      
+      
+def setup(bot):
+  bot.add_cog(Miscellaneous(bot))
