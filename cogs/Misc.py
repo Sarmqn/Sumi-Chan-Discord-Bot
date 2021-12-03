8d72bad2471440c48078e3acff9ff56b3314f068
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-12-03 12:34:06 +0000

    spelling error lmfao

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class Micellaneous (commands.Cog, name = "Miscellaneous"):
+class Miscellaneous (commands.Cog, name = "Miscellaneous"):
   """
   Miscellaneous commands
   """
