fced69c78afc556818c534b0b6057e19ccb5b7d7
Sarmqn <74299276+Sarmqn@users.noreply.github.com>
2021-12-03 15:22:33 +0000

    indentation error i think???

--- cogs/misc.py
+++ cogs/misc.py
@@ -8,15 +8,15 @@ class Miscellaneous(commands.Cog, name = " 👻 Miscellaneous"):
   def __init__ (self, bot):
     self.bot = bot
     
-    @commands.command(description="Displays the user's profile picture!", aliases = ["dp", "av", "pfp"])
-    async def avatar(self, ctx, *, dp: discord.Member=None):
-      URL = dp.avatar_url
-      await ctx.send(URL)
+  @commands.command(description="Displays the user's profile picture!", aliases = ["dp", "av", "pfp"])
+  async def avatar(self, ctx, *, dp: discord.Member=None):
+    URL = dp.avatar_url
+    await ctx.send(URL)
       
-    @commands.command(decription = "Changes the nickname for the tagged user within this server!", aliases = ["nickname", "Nick", "changename", "name"])
-    async def nick(ctx, nick, member: discord.Member=None):
-      await member.edit(nick=nick)
-      await ctx.send(f"{member.mention}'s nickname in {ctx.guild.name} has been changed")
+  @commands.command(decription = "Changes the nickname for the tagged user within this server!", aliases = ["nickname", "Nick", "changename", "name"])
+  async def nick(ctx, nick, member: discord.Member=None):
+    await member.edit(nick=nick)
+    await ctx.send(f"{member.mention}'s nickname in {ctx.guild.name} has been changed")
       
       
       
