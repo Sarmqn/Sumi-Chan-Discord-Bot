39e5a1b9a823be4e45ce506a61b79234b09e3b7e
Dawood <83209168+14685-562@users.noreply.github.com>
2021-12-06 09:34:36 +0000

    Update entertainment.py

--- cogs/entertainment.py
+++ cogs/entertainment.py
@@ -2,12 +2,13 @@ import discord
 from discord.ext import commands
 import random
 
-slapGIFs = ["https://cdn.discordapp.com/attachments/737096050598346866/906223201166704640/ehe_te_nandayo.png"]
+SlapGIFs = [""]
 HugGIFs = [""]
 KissGIFs = [""]
 SmilingGIFs = [""]
 TickleGIFs = [""]
-HugGIFs = [""]
+
+
 
 class Entertainment(commands.Cog, name="💣 Entertainment"):
     """
@@ -15,32 +16,35 @@ class Entertainment(commands.Cog, name="💣 Entertainment"):
     """
     def __init__(self, bot): # Defining the constructor method
         self.bot = bot
-
+    
+    async def user_check(self, user):
+        if (user is None) or (not isinstance(user, discord.Member)) or (not isinstance(user, discord.User)):
+            return "themselves"
+        else:
+            return user.mention
+    
+    
     @commands.command(description="Slap a user!")
     async def slap(self, ctx, *, user: discord.Member=None):
         print(type(user))
-        if (user is None) or (not isinstance(user, discord.Member)) or (not isinstance(user, discord.User)):
-            user = "themselves"
-        else:
-            user = user.mention
+        user = user_check(user)
         slapEmbed = discord.Embed(title= "Ouch!", description=f"{ctx.author} slapped {user}!", colour=discord.Color.from_rgb(241,210,231))
         slapEmbed.set_thumbnail(url=random.choice(slapGIFs))
         await ctx.reply(embed=slapEmbed, mention_author=False)
     
     
     """
-    
     --- WIP---
     @commands.command(description = "Hugs the tagged user!")
     async def hug(self, ctx, *, user:discord.Member=None):
+        user = user_check(user)
         
         
-        
-        await ctx.reply(embed=, mention_author=False)
+        await ctx.reply(embed=HugEmbed, mention_author=False)
     
     @commands.command(description = "Kisses the tagged user!")
     async def kiss(self, ctx, *, user:dicsord.Member=None):  
-        
+        user = user_check(user)
         
         
         await ctx.reply(embed=, mention_author=False)
@@ -48,21 +52,21 @@ class Entertainment(commands.Cog, name="💣 Entertainment"):
         
     @commands.command(description = "Smiles at the tagged user!")
     async def smile(self, ctx, *, user:discord.Member=None):  
-        
+        user = user_check(user)
         
         
         await ctx.reply(embed=, mention_author=False)
     
     @commands.command(description = "Tickles the tagged user!")
     async def tickle(self, ctx, *, user:discord.Member=None):  
-        
+        user = user_check(user)
         
         
         await ctx.reply(embed=, mention_author=False)
     
-    @commands.command(description = "Holds Hands with the tagged user!")
-    async def hh(self,ctx, *, user:discord.Member=None): 
-        
+    @commands.command(aliases = ['handhold', 'hh'], description = "Holds Hands with the tagged user!")
+    async def holdhands(self,ctx, *, user:discord.Member=None): 
+        user = user_check(user)
         
         
         await ctx.reply(embed=, mention_author=False)
