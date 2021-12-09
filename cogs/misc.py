cc612541c2f8425472004bacc0615d729a00e173
Dawood <83209168+14685-562@users.noreply.github.com>
2021-12-09 14:18:16 +0000

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -18,12 +18,12 @@ class Miscellaneous(commands.Cog, name = "👻 Miscellaneous"): # Creating a cla
         else:
             member = ctx.author # member is the user who sent the message
         avatarEmbed = discord.Embed(title=f"**{member}**'s Avatar", colour=discord.Color.from_rgb(241,210,231))
-        avatarEmbed.set_thumbnail(url=member.avatar_url)
+        avatarEmbed.set_image(url=member.avatar_url)
         avatarEmbed.set_author(name="User Avatar", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=avatarEmbed) # Sends the of the member to post in chat
       
       
-    """
+
     @commands.command(decription = "Changes the nickname for the tagged user within this server!", aliases = ["nickname", "changename", "name"])
     @commands.has_permissions(change_nickname=True)
     async def nick(ctx, member: discord.Member=None, *, nick=None): # Nickname command
@@ -38,7 +38,7 @@ class Miscellaneous(commands.Cog, name = "👻 Miscellaneous"): # Creating a cla
                 original_nick = member.nick
                 await member.edit(nick=nick) # member edits nickname
                 await ctx.send(f"{member.mention}'s nickname has been changed from {original_nick} to {nick}.") # send in chat that nickname has been changed
-    """    
+
       
       
 def setup(bot):
