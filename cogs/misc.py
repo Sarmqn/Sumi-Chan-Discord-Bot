0fe194b63916a5ef29fb6512fca1b8a30f3b3a0d
Dawood <83209168+14685-562@users.noreply.github.com>
2021-12-11 09:57:03 +0000

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -26,7 +26,7 @@ class Miscellaneous(commands.Cog, name = "👻 Miscellaneous"): # Creating a cla
 
     @commands.command(description = "Changes the nickname for the tagged user within this server!", aliases = ["nickname", "changename", "name"])
     @commands.has_permissions(change_nickname=True)
-    async def nick(ctx, member: discord.Member, nick): # Nickname command
+    async def nick(self, ctx, member: discord.Member=None, *, nick=None): # Nickname command
         print(member)
         print(nick)
         try:
@@ -34,9 +34,9 @@ class Miscellaneous(commands.Cog, name = "👻 Miscellaneous"): # Creating a cla
         except:
             pass
         else:
-            if (nick == '') and (member is None):
+            if (nick is None) and (member is None):
                 await ctx.send("Please mention a user and state a nickname!")
-            elif (nick == '') or (nick is None):
+            elif (nick is None) or (nick is None):
                 await ctx.send("Please specify a nickname!")
             else:
                 if (isinstance(member, discord.Member) or isinstance(member, discord.User)) and (len(nick.strip()) <= 32):
